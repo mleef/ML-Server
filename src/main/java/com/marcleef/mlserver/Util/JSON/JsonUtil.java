405b74cbbab4832f0d4237f2c7dc051c4709364f
Marc Leef <mleef@uoregon.edu>
2015-04-27 18:37:21 -0700

    Updated JSONUtil

---
+++ src/main/java/com/marcleef/mlserver/Util/JSON/JsonUtil.java
@@ -0,0 +1,16 @@
+package com.marcleef.mlserver.Util.JSON;
+import com.google.gson.*;
+import spark.ResponseTransformer;
+/**
+ * Created by marc_leef on 4/19/15.
+ * Convert SPARK return values to JSON by overring render method.
+ */
+public class JsonUtil implements ResponseTransformer {
+    private Gson gson = new Gson();
+
+    @Override
+    public String render(Object model) {
+        return gson.toJson(model);
+    }
+
+}
