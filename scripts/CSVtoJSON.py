a90b46d92b145016b53dab8f8c1d1bc9f6dc4034
Marc Leef <mleef@uoregon.edu>
2015-05-05 10:39:57 -0700

    Implemented helper Python script for .csv to .json conversion of training and test sets.

--- scripts/CSVtoJSON.py
+++ scripts/CSVtoJSON.py
@@ -1,27 +1,41 @@
 #!/usr/bin/env python
+import sys
 
 
+def toJSON(f, token, query):
+    try:
+        file = open(f)
+        lines = [line.strip() for line in file]
+        attributes = "["
+        examples = "["
+        for attr in lines[0].split(","):
+            attributes += '"' + attr + '", '
+
+        for i in range(1, len(lines)):
+            examples += '"' + lines[i] + '", '
+
+        examples = examples[:-2]
+        examples += "]"
+        attributes = attributes[:-2]
+        attributes += "]"
+        if(query):
+            return '{{"token" : {0}, "examples" : {1}}}'.format('"' + token + '"', examples)
+        else:
+            return '{{"token" : {0}, "attributes" : {1}, "examples" : {2}}}'.format('"' + token + '"', attributes, examples)
+
+    finally:
+        file.close()
 
 def main(argv = sys.argv):
-	JSON = '{"token" : '
+    if(len(argv) < 3):
+        print "Usage: python CSVtoJSON.py [token] [path/to/csv/data]"
+        return
+    elif(len(argv) > 3):
+        if(argv[3] == "-query"):
+            print(toJSON(argv[1], argv[2], True))
+            return
 
-    # Get file to parse
-    f = open argv[0]
-
-    # Get token
-    token = argv[1]
-    JSON += '"' + token '", "attributes" : ['
-    
-    try:
-    	lines = [line.strip() for line in f]
-    	for(attr in lines[0].split(","):
-    		JSON += '" ' + attr + ',"
-    	JSON = JSON[:-1]
-    	JSON += '], "examples" : [ 
-	finally:
-    	f.close()
-
-
+    print(toJSON(argv[1], argv[2], False))
 
 if __name__ == "__main__":
     try:
